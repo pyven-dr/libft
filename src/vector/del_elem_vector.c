ae65eea242db289156dd9202d584c2b062998f24
Pablo Yven Drogrey <pyven-dr@student.42lyon.fr>
2024-04-11 18:40:29 +0200

    add function pointer to vector functions to free element

--- src/vector/del_elem_vector.c
+++ src/vector/del_elem_vector.c
@@ -6,26 +6,27 @@
 /*   By: pyven-dr <pyven-dr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:37:03 by pyven-dr          #+#    #+#             */
-/*   Updated: 2024/04/10 15:34:02 by pyven-dr         ###   ########.fr       */
+/*   Updated: 2024/04/09 16:37:03 by pyven-dr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	del_elem_vector(t_vector *vector, size_t index)
+int	del_elem_vector(t_vector *vector, size_t index, void(*ft_free) (void *elem))
 {
 	if (index > vector->size)
 		return (1);
 	ft_bzero(vector->elements + index * vector->elem_size, vector->elem_size);
 	while (index < vector->size)
 	{
-		ft_memcpy(vector->elements + index * vector->elem_size ,\
+		ft_memcpy(vector->elements + index * vector->elem_size, \
 				vector->elements + (index + 1) * vector->elem_size, \
 				vector->elem_size);
 		index++;
 	}
 	if (get_elem_vector(vector, index) != NULL)
-		free(vector->elements + index * vector->elem_size);
+		if (ft_free != NULL)
+			ft_free(vector->elements + index * vector->elem_size);
 	vector->size--;
 	return (0);
 }
